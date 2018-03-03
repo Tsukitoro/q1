20dd24c45fd1b3a35af0af1fb3ce363d64d786b8
Tsukitoro <36997697+Tsukitoro@users.noreply.github.com>
2018-03-03 16:27:04 +0300

    Update q13.php

--- q13.php
+++ q13.php
@@ -1,95 +1,166 @@
 <?php 
+interface Shape 
+{
+  
+	public function calcArea();
 
-
-
-
-
-interface Shape {
-  public function calcArea();
 }
 
-class Circle implements Shape {
-  private $radius;
-   
-  public function __construct($radius)
-  {
-    $this -> radius = $radius;
-  }
-  public function calcArea()
-  {
-    return $this -> radius * $this -> radius * pi();
-  }
+
+
+class Circle implements Shape 
+{
+  
+	private $radius;
+  
+  
+	public function __construct($radius)
+	{
+		$this->radius = $radius;
+	}
+  
+	public function calcArea()
+  
+	{
+    
+		return $this -> radius * $this -> radius * pi();
+  
+	}
+
 }
 
-class Rectangle implements Shape {
-  private $width;
-  private $height;
+
+
+class Rectangle implements Shape 
+{
+  
+	private $width;
+  	private $height;
    
-  public function __construct($width, $height)
-  {
-    $this -> width = $width;
-    $this -> height = $height;
-  }
+  
+	public function __construct($width, $height)
+ 
+	{
+    
+		$this -> width = $width;
+  		$this -> height = $height;
+  
+	}
       
-  public function calcArea()
-  {
-    return $this -> width * $this -> height;
-  }
+ 
+ 	public function calcArea()
+  
+	{
+    
+		return $this -> width * $this -> height;
+  
+	}
+
 }
 
-class Triangle implements Shape {
-  private $a;
-  private $b;
-  private $c;
+
+
+class Triangle implements Shape 
+{
+  
+	private $a;
+  	private $b;
+  	private $c;
    
-  public function __construct($a, $b , $c)
-  {
-    $this -> a = $a;
-    $this -> b = $b;
-    $this -> c = $c;
-  }
+  
+	public function __construct($a, $b , $c)
+  
+	{
+    
+		$this -> a = $a;
+   	 	$this -> b = $b;
+    		$this -> c = $c;
+  
+	}
       
-  public function calcArea()
-  {
-    $p = (float) (($this -> a + $this -> b + $this -> c) / 2);
-    return  sqrt($p * ($p - $this -> a) * ($p - $this -> b) * ($p - $this -> c));
-  }
+  
+	public function calcArea()
+  
+	{
+    
+		$p = (float) (($this -> a + $this -> b + $this -> c) / 2);
+ 
+ 		return  sqrt($p * ($p - $this -> a) * ($p - $this -> b) * ($p - $this -> c));
+  
+	}
+
 }
+
+
 
 $array_shaps = array();
 
+
+
 $json = file_get_contents('./figures.json');
+
 
 $json_data = json_decode($json,true);
 
-foreach ($json_data as $value1) {
+
+foreach ($json_data as $value1) 
+{
+	
 	switch($value1)
-  {
-    case "circle":
-      array_push($array_shaps, new Circle());
-    case "rectangle":
-      array_push($array_shaps, new Rectangle());
-    case "triangle":
-      array_push($array_shaps, new Triangle());
-  }
+  
+	{
+    
+	case "circle":
+      
+		array_push($array_shaps, new Circle($value2 -> radius));
+    
+	case "rectangle":
+      
+		array_push($array_shaps, new Rectangle($value2 -> width, $value2 -> height));
+    
+	case "triangle":
+      
+		array_push($array_shaps, new Triangle($value2 -> a, $value2 -> b, $value2 -> c));
+  
+	}
+
 }
+
+
 
 usort($array_shaps, function($a, $b)
 {
-    return $a->calcArea() < $b->calcArea();
+    
+	return $a->calcArea() < $b->calcArea();
 });
 
+
+
 foreach ($array_shaps as $key => $value2)
+
 {
-    echo "{$key} => {$value2} ";
-    switch($value1)
-    {
-      case "circle":
-        echo "Circle, Radius = {$value2->radius}.\n";
-      case "rectangle":
-        echo "Rectangle, Height = {$value2->height} , Width = {$value2->width}.\n";
-      case "triangle":
-        echo "Triangle, A = {$value2->a} , B = {$value2->b} , C = {$value2->c}.\n";
-    }
+   
+ 	echo "{$key} => {$value2} ";
+    
+	switch($value1)
+    
+	{
+      
+		case "circle":
+        
+			echo "Circle, Radius = {$value2->radius}.\n";
+      
+		case "rectangle":
+        
+			echo "Rectangle, Height = {$value2->height} , Width = {$value2->width}.\n";
+      
+		case "triangle":
+        
+			echo "Triangle, A = {$value2->a} , B = {$value2->b} , C = {$value2->c}.\n";
+    
+	}
+
 }
+
+
 ?>
